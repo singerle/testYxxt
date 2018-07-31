7f0e42cd5a6436799bafd4f1b3c9b37446d172a0
lixin <1411341896@qq.com>
2018-07-31 14:27:41 +0800

    Signed-off-by: lixin <1411341896@qq.com>

---
+++ static/config.js
@@ -0,0 +1,8 @@
+const config = {
+	// url: 'http://192.168.0.140:8086/enroll/'
+	// url: 'http://zsyx.ctbu.edu.cn:80/enroll/'
+	// url:'http://www.yunhuakeji.com:10071/enroll/',
+	// url: 'http://202.202.144.153:8082/enroll/'
+	//url: 'http://yx.cqut.edu.cn/enroll/'
+	url: 'http://222.179.45.246:8083'
+}
